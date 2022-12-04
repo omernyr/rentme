0b2e5397c2e8085c69a4ea7557e33f8a16eb256f
Ömer NAYIR <omer.nayir.mdbf17@iste.edu.tr>
2022-12-04 16:34:15 +0300

    api files upgraded

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,41 +5,54 @@ import { useState } from 'react';
 
 const Search = ({ searchCity, setSearchCity }) => {
 
+  const [didSelectDate, setDidSelectDate] = useState({
+    pickup: "2022-12-14",
+    dropoff: null
+  });
+  
   const [datas, setDatas] = useState([]);
   const [cityLocation, setCityLocation] = useState({
     latitude: 0,
     longitude: 0
   })
 
-  const handleFetch = (e) => {
-
-    e.preventDefault()
+  const handleChange = (e) => {
     setSearchCity(e.target.value)
+  }
 
-    fetch(`https://booking-com.p.rapidapi.com/v1/car-rental/search?pick_up_longitude=14.421133&from_country=sk&pick_up_latitude=50.08773&currency=AED&drop_off_datetime=2023-02-27%2009%3A00%3A00&locale=en-gb&sort_by=recommended&drop_off_latitude=50.08773&pick_up_datetime=2023-02-26%2009%3A00%3A00&drop_off_longitude=14.421133`, options)
+  const handleFetch = (e) => {
+    e.preventDefault()
+
+    console.log("pickup date ->", didSelectDate.pickup);
+    console.log("dropoff date ->", didSelectDate.dropoff);
+
+    // for about rent a car info
+    fetch(`https://booking-com.p.rapidapi.com/v1/car-rental/search?pick_up_longitude=${cityLocation.longitude}&from_country=sk&pick_up_latitude=${cityLocation.latitude}&currency=AED&drop_off_datetime=${didSelectDate.dropoff}%2009%3A00%3A00&locale=en-gb&sort_by=recommended&drop_off_latitude=${cityLocation.latitude}&pick_up_datetime=${didSelectDate.pickup}%2009%3A00%3A00&drop_off_longitude=${cityLocation.longitude}`,
+      options)
       .then(response => response.json())
       .then(response => console.log("My data ->", response))
 
+    // my city long and lat
     fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/cities?minPopulation=1000000&namePrefix=${searchCity}`, geoApiOptions)
       .then(response => response.json())
-      .then(response => console.log("my city ->", setCityLocation({ latitude: response.data[0].latitude }), setCityLocation({ longitude: response.data[0].longitude })))
+      .then(response => console.log("my city ->",
+        setCityLocation({ longitude: response.data[0].longitude, latitude: response.data[0].latitude })
+      ))
   }
   return (
     <div className='search'>
-      <img src="https://s7d1.scene7.com/is/image/hyundai/2022-santa-fe-phev-limited-awd-shimmering-silver:Vehicle-Carousel?fmt=webp-alpha" alt="" />
-      <h1>
-        Search, book and rent
-        vehicle easily
-      </h1>
-      <form className='search-vehicle-form'>
+      <form onSubmit={handleFetch} className='search-vehicle-form'>
         <span><CiLocationOn /></span>
         <input
+          onChange={handleChange}
           value={searchCity}
-          onChange={handleFetch}
-          className='search-vehicle-input' type="text"
+          className='search-vehicle-input'
           placeholder='Esenler, Istanbul' />
-        <button onClick={handleFetch} type='submit' className='search-vehicle-button'><FiSearch /></button>
+        <input type="date" className='date' onChange={(e) => setDidSelectDate({ pickup: e.target.value })} name='pickup' />
+        <input type="date" className='date' onChange={(e) => setDidSelectDate({ dropoff: e.target.value })} name='dropoff' />
+        <button type='submit' className='search-vehicle-button'><FiSearch /></button>
       </form>
+
     </div>
   )
 }
